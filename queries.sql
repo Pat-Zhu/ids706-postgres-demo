244a8786483a3470965d7a9528f3a615b11d98f5
Pat-Zhu <xihanz0108@outlook.com>
2025-10-02 02:16:56 -0400

    Finish queries.sql with practice code

--- queries.sql
+++ queries.sql
@@ -7,3 +7,31 @@ SELECT name, cuisine, distance_miles, rating
 FROM restaurants
 WHERE distance_miles <= 5 AND rating >= 4.0
 ORDER BY rating DESC, distance_miles ASC;
+
+SELECT
+  name,
+  distance_miles
+FROM restaurants
+WHERE distance_miles <= 2.0
+ORDER BY distance_miles ASC, name ASC;
+
+SELECT
+  name,
+  rating
+FROM restaurants
+ORDER BY rating DESC, name ASC
+LIMIT 3;
+
+SELECT
+  name,
+  avg_cost,
+  ROUND(avg_cost * 1.075, 2) AS cost_with_tax
+FROM restaurants
+ORDER BY name ASC;
+
+SELECT
+  cuisine,
+  COUNT(*) AS restaurant_count
+FROM restaurants
+GROUP BY cuisine
+ORDER BY restaurant_count DESC, cuisine ASC;
